52ead572510fa07740a833ab950618e9f053393b
Ubuntu <ubuntu@ip-172-31-39-6.us-west-2.compute.internal>
2013-07-10 11:28:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer ("I'm a string!");
+//var buffer = new Buffer ("I'm a string!");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString);
+  response.send("Hello from index.html");
 });
 
 var port = process.env.PORT || 5000;
